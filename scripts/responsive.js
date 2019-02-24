647591588874e25246e1904ef5840eb306280638
Aaron Santiago <aaronsantiago@Aarons-MacBook-Pro.local>
2019-02-24 18:52:20 -0500

    update layout with new image and better responsivity

--- scripts/responsive.js
+++ scripts/responsive.js
@@ -1,6 +1,7 @@
 window.onload = function() {
   var sidebar = document.getElementById("sidebar");
   var sidebarButton = document.getElementById("sidebar-button");
+  var sidebarImg = document.getElementById("sidebar-img");
 
   function openSidebar(e) {
     if (sidebar.className.indexOf("slideIn") != -1) {
@@ -10,5 +11,5 @@ window.onload = function() {
     }
   }
 
-  sidebarButton.addEventListener("click", openSidebar, false);
+  sidebarImg.addEventListener("click", openSidebar, false);
 };
